b89264dfb01bddf302534edc89f17d95e467b949
MohammadReza0044 <you@example.com>
2022-04-16 18:31:11 +0430

    yes in dice is ok

--- rollingDice.py
+++ rollingDice.py
@@ -1,2 +1,9 @@
 import random
-print("your number is:", (random.randint(1,6)) )
+from timeit import repeat
+
+
+while repeat:
+    print("your number is:", (random.randint(1,6)) )
+    print("do you want to play again? yes or no:  ")
+    repeat="yes".lower() in input()
+   
