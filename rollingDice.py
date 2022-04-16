b60db9ca5765b594de0d2ccc33b1120ab5e3c330
MohammadReza0044 <you@example.com>
2022-04-16 18:54:35 +0430

    dice has been completed

--- rollingDice.py
+++ rollingDice.py
@@ -6,4 +6,7 @@ while repeat:
     print("your number is:", (random.randint(1,6)) )
     print("do you want to play again? yes or no:  ")
     repeat="yes".lower() in input()
+else:
+    print("okay, it was a good game")
    
+
